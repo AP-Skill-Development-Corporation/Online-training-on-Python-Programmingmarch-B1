45eb2770cae648bfaea236a154a344514ab4d65e
girisha <girisha.devara@gmail.com>
2021-03-09 13:50:04 +0530

    Day 7 content(file handling) added

---
+++ Day_07_08March2021/samplemodule.py
@@ -0,0 +1,15 @@
+def minus(a1,a2):
+    return(a1-a2)
+    
+def evenodd(s1):
+    if s1%2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+        
+def summation(z1,z2):
+    return(z1+z2)
+
+def divide(z1,z2):
+    return(z1/z2)
